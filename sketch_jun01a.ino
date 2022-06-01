b121f4689edebd5f8e69ff8893a80b8d798a56f6
ton5556 <74088689+ton5556@users.noreply.github.com>
2022-06-01 21:48:57 +0700

    Add files via upload

---
+++ sketch_jun01a.ino
@@ -0,0 +1,55 @@
+#include <Servo.h>
+Servo servo;
+int trigPin = 5;
+int echoPin = 6;
+int servoPin = 7;
+long duration, dist, average;
+long aver[3];
+const int Relay = 2;
+
+void setup() {
+  Serial.begin(9600);
+  servo.attach(servoPin);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  servo.write(0);
+  delay(100);
+  servo.detach();
+  pinMode(Relay, OUTPUT);
+}
+
+void measure() {
+  digitalWrite(10, HIGH);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(15);
+  digitalWrite(trigPin, LOW);
+  pinMode(echoPin, INPUT);
+  duration = pulseIn(echoPin, HIGH);
+  dist = (duration / 2) / 29.1;
+}
+void loop() {
+  for (int i = 0; i <= 2; i++) {
+    measure();
+    aver[i] = dist;
+    delay(10);
+  }
+  dist = (aver[0] + aver[1] + aver[2]) / 3;
+
+  if ( dist < 50 ) {
+    digitalWrite(Relay, HIGH);
+    servo.attach(servoPin);
+    delay(1);
+    servo.write(0);
+    delay(3000);
+    servo.write(150);
+    delay(1000);
+    servo.detach();
+  }
+  else {
+    digitalWrite(Relay, LOW); 
+  }
+  delay(500);
+  Serial.print(opens);
+}
